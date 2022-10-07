b46c88fb4f616b11b929d60d62662aa6a9ad4bfa
gogins <michael.gogins@gmail.com>
2022-10-07 11:28:55 -0400

    Cleanup.

--- update-dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Updating dependencies..."
-pwd
-cp ~/csound-wasm/dist-wasm/*.js pieces/
-cp -rf ~/csound-ac/patches/*.inc pieces/patches/
-cp -rf ~/csound-ac/silencio/js/*.js pieces/silencio/js/
-cp -rf ~/csound-ac/silencio/css/*.css pieces/silencio/css/
-find pieces -ls
