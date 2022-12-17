47dc5217ca324abf79a09b9df6cce8cf039bfcdb
gogins <michael.gogins@gmail.com>
2022-12-17 11:22:35 -0500

    npm-ify index and main.

---
+++ main.js
@@ -0,0 +1,18 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">Vite</a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
