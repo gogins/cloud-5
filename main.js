779d9428177dbe8597238aaaafe76edc045deb1d
gogins <michael.gogins@gmail.com>
2022-12-18 12:16:07 -0500

    Corrected bad svg tag syntax.

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">Vite</a>
+    <a href="https://vitejs.dev" target="_blank"><img src="/vite.svg" class="logo vanilla" alt="Vite logo" /></a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>cloud-music</h1>
+      <p>This index.html file is kind of like a makefile for cloud-music.
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
