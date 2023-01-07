6ff9b2f0d9b8150f789cc981bb1a518bf8ef1932
gogins <michael.gogins@gmail.com>
2023-01-06 20:47:13 -0500

    Updated Strudel from origin.

--- patch-strudel.py
+++ patch-strudel.py
@@ -21,3 +21,14 @@ with open(Repl_jsx_filepath, "r+") as file:
   file.seek(0)
   file.write(patched_text)
   
+csound_mjs_filepath = "strudel/packages/csound/csound.mjs"
+print(f"Patching '{csound_mjs_filepath}'")
+with open(csound_mjs_filepath, "r+") as file:
+  find_this = '''i ${p1} ${p2} ${p3} ${p4} ${p5} "${p6}"'''
+  replace_with = '''i ${p1} ${p2} ${p3} ${p4} ${p5}'''
+  text = file.read()
+  patched_text = text.replace(find_this, replace_with)
+  print(patched_text)
+  file.seek(0)
+  file.write(patched_text)
+  
