4d3616ca10d78ee084c12c3fe1c8d872b105105a
gogins <michael.gogins@gmail.com>
2023-03-02 21:14:28 -0500

    Removing Strudel.

--- docs/chunks/index.d02d4673.js
+++
@@ -1,1 +0,0 @@
-import{c as e,r,a as t,s as c,b as n,t as g,v as p,d}from"./voicings.8325b92a.js";import"./prebake.5a4b2523.js";import"./index.ee58e7ae.js";export{e as addVoicings,r as rootNotes,t as scale,c as scaleTranspose,n as setVoicingRange,g as transpose,p as voicingRegistry,d as voicings};
