67fb0b4fe46267c442fe1c1302c62a37806118db
gogins <michael.gogins@gmail.com>
2024-10-09 13:53:09 +0200

    Cleaning up build logic (#73).

--- csound-node-prefix/build-outdated-csound-node.sh
+++ csound-node-prefix/build-outdated-csound-node.sh
@@ -1,8 +1,29 @@
+echo "Current working directory:" ; pwd
+CMAKE_SOURCE_DIR=$1
+echo "CMAKE_SOURCE_DIR: ${CMAKE_SOURCE_DIR}"
 pwd
-if [[ ! -s csound-node-updated.txt && ! -n "$REBUILD" ]]; then 
+
+function rebuild () {
+    node-gyp rebuild
+    bash -c "cp ${CMAKE_SOURCE_DIR}/csound-node-prefix/src/csound-node/build/Release/csound.node ${CMAKE_SOURCE_DIR}/"
+}
+
+if [[ ! -f ${CMAKE_SOURCE_DIR}/csound-node-prefix/src/csound-node/build/Release/csound.node ]]
+then
+    echo "A target is missing, rebuilding csound-node..."
+    rebuild
+    exit 
+elif [[ ! -z "$REBUILD" ]]
+then
+    echo "Manually rebuilding csound-node...";
+    rebuild
+    exit 
+elif [[ -s ${CMAKE_SOURCE_DIR}/csound-node-prefix/csound-node-updated.txt ]]
+then 
+    echo "Sources were outdated, rebuilding csound-node...";
+    rebuild
+else 
     echo "csound-node sources are up to date.";
     exit 0; 
-else 
-    echo "csound-node sources were outdated, rebuilding...";
-    node-gyp rebuild; 
 fi
+
